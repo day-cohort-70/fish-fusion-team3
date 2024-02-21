35a3555a50848c51ff7625bd007e63ac627292b7
Justin-Acosta <jacostaprof@gmail.com>
2024-02-21 13:53:16 -0600

    added annotation for boat inventory and monger

---
+++ fishMonger.js
@@ -0,0 +1,24 @@
+const { boatInventory } = require("./fishingBoat.js")
+
+/*
+
+const mongerInventory = () => {
+// create variable to todaysCatch
+const todaysCatch = boatInventory()
+
+// create variable for monger purchases
+let mongerPurchases = []
+
+
+
+// check todaysCatch for prices <= 7.5 
+// check if species amount >= 10
+// push to mongerPurchases array
+// return value of mongerPurchases
+}
+
+mongerInventory()
+
+*/
+
+module.exports { mongerInventory }
